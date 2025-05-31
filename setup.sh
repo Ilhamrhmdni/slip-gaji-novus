39554444d3a3b4e72ba92cfe53cb5a5cee05a89d
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-05-31 20:43:31 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,8 +2,8 @@
 # Instal wkhtmltopdf
 apt-get update && apt-get install -y wkhtmltopdf
 
-# Instal dependensi tambahan jika diperlukan
-apt-get install -y xvfb libfontconfig1 libxrender1
+# Instal dependensi tambahan (penting untuk menjalankan wkhtmltopdf headless)
+apt-get install -y xvfb libfontconfig1 libxrender1 libssl1.1
 
-# Instal requirements Python
+# Instal library Python dari requirements.txt
 pip install -r requirements.txt
