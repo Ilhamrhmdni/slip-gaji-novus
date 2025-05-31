6cf57828664408e32d2d7a1405f5902bdf1642e8
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-06-01 00:28:06 +0700

    Update database.py

--- database.py
+++ database.py
@@ -1,4 +1,3 @@
-# database.py
 import sqlite3
 
 DB_NAME = "karyawan.db"
@@ -6,6 +5,7 @@ DB_NAME = "karyawan.db"
 def init_db():
     conn = sqlite3.connect(DB_NAME)
     c = conn.cursor()
+
     # Tabel Users
     c.execute('''
         CREATE TABLE IF NOT EXISTS users (
@@ -15,6 +15,7 @@ def init_db():
             role TEXT
         )
     ''')
+
     # Tabel Karyawan
     c.execute('''
         CREATE TABLE IF NOT EXISTS karyawan (
@@ -30,11 +31,31 @@ def init_db():
             alamat_bank TEXT
         )
     ''')
+
+    # Tabel Gaji
+    c.execute('''
+        CREATE TABLE IF NOT EXISTS gaji (
+            karyawan_id INTEGER PRIMARY KEY,
+            gaji_pokok INTEGER,
+            tunjangan_kinerja INTEGER,
+            tunjangan_makan INTEGER,
+            tunjangan_overtime INTEGER,
+            tunjangan_jabatan INTEGER,
+            pph21 INTEGER,
+            bpjs_kesehatan INTEGER,
+            bpjs_ketenagakerjaan INTEGER,
+            tagihan_hutang1 INTEGER,
+            tagihan_hutang2 INTEGER,
+            FOREIGN KEY(karyawan_id) REFERENCES karyawan(id)
+        )
+    ''')
+
     # User default
     c.execute("SELECT * FROM users WHERE username='admin'")
     if not c.fetchone():
         c.execute("INSERT INTO users (username, password, role) VALUES (?, ?, ?)",
                   ("admin", "1234", "admin"))
+
     conn.commit()
     conn.close()
 
@@ -75,3 +96,50 @@ def get_karyawan_by_id(karyawan_id):
     data = c.fetchone()
     conn.close()
     return data
+
+def get_gaji_by_id(karyawan_id):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute('''
+        SELECT gaji_pokok, tunjangan_kinerja, tunjangan_makan, tunjangan_overtime,
+               tunjangan_jabatan, pph21, bpjs_kesehatan, bpjs_ketenagakerjaan,
+               tagihan_hutang1, tagihan_hutang2
+        FROM gaji WHERE karyawan_id = ?
+    ''', (karyawan_id,))
+    data = c.fetchone()
+    conn.close()
+    return data
+
+def save_or_update_gaji(karyawan_id, data):
+    conn = sqlite3.connect(DB_NAME)
+    c = conn.cursor()
+    c.execute("SELECT 1 FROM gaji WHERE karyawan_id = ?", (karyawan_id,))
+    exists = c.fetchone()
+
+    if exists:
+        c.execute('''
+            UPDATE gaji SET 
+                gaji_pokok = ?, tunjangan_kinerja = ?, tunjangan_makan = ?, tunjangan_overtime = ?,
+                tunjangan_jabatan = ?, pph21 = ?, bpjs_kesehatan = ?, bpjs_ketenagakerjaan = ?,
+                tagihan_hutang1 = ?, tagihan_hutang2 = ?
+            WHERE karyawan_id = ?
+        ''', (
+            data["gaji_pokok"], data["tunjangan_kinerja"], data["tunjangan_makan"], data["tunjangan_overtime"],
+            data["tunjangan_jabatan"], data["pph21"], data["bpjs_kesehatan"], data["bpjs_ketenagakerjaan"],
+            data["tagihan_hutang1"], data["tagihan_hutang2"], karyawan_id
+        ))
+    else:
+        c.execute('''
+            INSERT INTO gaji (
+                karyawan_id, gaji_pokok, tunjangan_kinerja, tunjangan_makan, tunjangan_overtime,
+                tunjangan_jabatan, pph21, bpjs_kesehatan, bpjs_ketenagakerjaan,
+                tagihan_hutang1, tagihan_hutang2
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+        ''', (
+            karyawan_id, data["gaji_pokok"], data["tunjangan_kinerja"], data["tunjangan_makan"],
+            data["tunjangan_overtime"], data["tunjangan_jabatan"], data["pph21"], data["bpjs_kesehatan"],
+            data["bpjs_ketenagakerjaan"], data["tagihan_hutang1"], data["tagihan_hutang2"]
+        ))
+
+    conn.commit()
+    conn.close()
