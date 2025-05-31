f7512e1b5e55862ed1cba81afe473f1ece8debf1
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-05-31 23:02:50 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,8 @@
+# utils.py
+from num2words import num2words
 
+def format_rupiah(angka):
+    return f"Rp {angka:,.0f}".replace(",", ".")
+
+def terbilang_rupiah(angka):
+    return num2words(angka, lang='id').capitalize() + " rupiah"
