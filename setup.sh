3a39464582eb268d42438afd04d9842791febaac
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-05-31 20:18:12 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,9 @@
+#!/bin/bash
+# Instal wkhtmltopdf
+apt-get update && apt-get install -y wkhtmltopdf
 
+# Instal dependensi tambahan jika diperlukan
+apt-get install -y xvfb libfontconfig1 libxrender1
+
+# Instal requirements Python
+pip install -r requirements.txt
