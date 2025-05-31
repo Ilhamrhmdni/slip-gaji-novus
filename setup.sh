7108cec61c63d33e26a018792835bf8033d92284
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-05-31 23:04:58 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,19 +1,3 @@
 #!/bin/bash
-
-# Instal dependensi
-apt-get update && apt-get install -y \
-    libfontconfig1 \
-    libxrender1 \
-    libx11-dev \
-    xfonts-75dpi \
-    xfonts-base
-
-# Download dan instal wkhtmltopdf secara manual
-curl -LO https://github.com/wkhtmltopdf/packaging/releases/download/0.12.6-1/wkhtmltox_0.12.6-1.focal_amd64.deb 
-dpkg -i wkhtmltox_0.12.6-1.focal_amd64.deb || true
-
-# Instal dependensi yang hilang (jika ada)
-apt-get install -f -y
-
-# Instal library Python
+apt-get update && apt-get install -y wkhtmltopdf xvfb libfontconfig1 libxrender1
 pip install -r requirements.txt
