04fd3423f2c918a2ff2946d36db170fa4c8d7d5e
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-06-08 18:02:13 +0700

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,8 +1,25 @@
 # utils.py
-from num2words import num2words
+def to_rupiah(x):
+    return f"Rp {x:,.0f}".replace(",", ".")
 
-def format_rupiah(angka):
-    return f"Rp {angka:,.0f}".replace(",", ".")
-
-def terbilang_rupiah(angka):
-    return num2words(angka, lang='id').capitalize() + " rupiah"
+def terbilang(n):
+    angka = ["", "satu", "dua", "tiga", "empat", "lima",
+             "enam", "tujuh", "delapan", "sembilan", "sepuluh", "sebelas"]
+    if n < 12:
+        return angka[n]
+    elif n < 20:
+        return terbilang(n - 10) + " belas"
+    elif n < 100:
+        return terbilang(n // 10) + " puluh " + terbilang(n % 10)
+    elif n < 200:
+        return "seratus " + terbilang(n - 100)
+    elif n < 1000:
+        return terbilang(n // 100) + " ratus " + terbilang(n % 100)
+    elif n < 2000:
+        return "seribu " + terbilang(n - 1000)
+    elif n < 1000000:
+        return terbilang(n // 1000) + " ribu " + terbilang(n % 1000)
+    elif n < 1000000000:
+        return terbilang(n // 1000000) + " juta " + terbilang(n % 1000000)
+    else:
+        return "terlalu besar"
