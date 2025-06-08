6eebbb5849a9e30bbee2620378d11877887c30ac
Ilhamrhmdni <130125045+Ilhamrhmdni@users.noreply.github.com>
2025-06-08 17:47:39 +0700

    Update auth.py

--- auth.py
+++ auth.py
@@ -1,23 +1,12 @@
 import streamlit as st
 
-def halaman_login():
-    st.title("🔐 Login Admin")
+def login():
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
 
     if st.button("Login"):
-        from database import login_user
-        user = login_user(username, password)
-        if user:
-            st.session_state.logged_in = True
-            st.session_state.role = user[3]  # Simpan peran (role) user
-            st.success("Login Berhasil!")
-            st.rerun()  # Muat ulang agar form login hilang
+        if username == "admin" and password == "admin":
+            return True
         else:
-            st.error("Username atau Password salah")
-
-def logout():
-    st.session_state.logged_in = False
-    st.session_state.role = None
-    st.info("Anda telah logout.")
-    st.rerun()
+            st.error("Username/password salah")
+    return False
